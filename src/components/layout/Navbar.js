04c37d00dd56eec0b430ee149962f967be901d0a
Mohamad kalaaji <technolaaji@gmail.com>
2019-08-03 21:50:29 +0300

    read me updated

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+const Navbar =  () => {
+    return (
+       <nav className="navbar navbar-dark bg-dark mb-5">
+          <Link to='/' className="navbar-brand mb-0 h1 mx-auto"><span >Lyric Finder</span></Link>
+       </nav>
+    )
+}
+
+export default Navbar;
